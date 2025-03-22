fc4a61330707d1b63709059d1e2f592a510bfb02
Adi Thakker <Aditya.Thakker@gmail.com>
2025-03-22 18:44:02 -0400

    WIP updates for inMemory

---
+++ SemanticKernel.Embeddings/Artifacts.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.VectorData;
+
+namespace SemanticKernel.Embeddings;
+
+internal sealed class Artifacts<TKey>
+{
+    [VectorStoreRecordKey]
+    public required TKey Key { get; set; }
+
+    [VectorStoreRecordData]
+    public required string Category { get; set; }
+
+    [VectorStoreRecordData]
+    public required string Text { get; set; }
+
+    [VectorStoreRecordVector(1536)]
+    public ReadOnlyMemory<float> TextEmbedding { get; set; }
+}
--- SemanticKernel.Embeddings/Program.cs
+++ SemanticKernel.Embeddings/Program.cs
@@ -1,32 +1,22 @@
-﻿using Microsoft.SemanticKernel;
-using Microsoft.SemanticKernel.Connectors.Milvus;
-using Microsoft.SemanticKernel.Connectors.Ollama;
+﻿using Microsoft.Extensions.VectorData;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.Connectors.InMemory;
 using Microsoft.SemanticKernel.Embeddings;
 using Microsoft.SemanticKernel.Memory;
-using OllamaSharp;
-
-// No Out of the box connecor for Milvus yet, so we need to manually configure it
-#pragma warning disable SKEXP0020 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-#pragma warning disable SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-
-var milvusStore = new MilvusMemoryStore("localhost", 19530);
-var memoryBuilder = new MemoryBuilder();
-memoryBuilder
-    .WithMemoryStore(milvusStore)
-    .WithTextEmbeddingGeneration(new OllamaApiClient(new Uri("http://localhost:11434"), "all-minilm").AsTextEmbeddingGenerationService());
-
-#pragma warning restore SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-#pragma warning restore SKEXP0020 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
-
+using SemanticKernel.Embeddings;
 
 var builder = Kernel
                 .CreateBuilder()
-                //.AddOllamaTextEmbeddingGeneration("all-minilm", new Uri("http://localhost:11434"))
-                .AddOllamaTextGeneration("phi3", new Uri("http://localhost:11434"));
-
+                .AddOllamaTextEmbeddingGeneration("all-minilm", new Uri("http://localhost:11434"))
+                .AddOllamaTextGeneration("phi3", new Uri("http://localhost:11434"))
+                .AddInMemoryVectorStore();
+                
 
 var kernel = builder.Build();
-var memory = memoryBuilder.Build();
+var memory = kernel.GetRequiredService<InMemoryVectorStore>();
+#pragma warning disable SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
+var embedding = kernel.GetRequiredService<IEmbeddingGenerationService<string, float>>();
+#pragma warning restore SKEXP0001 // Type is for evaluation purposes only and is subject to change or removal in future updates. Suppress this diagnostic to proceed.
 
 var instructions = await File.ReadAllTextAsync("Plugin/instructions.txt");
 var csvData = await File.ReadAllTextAsync("Plugin/data.csv");
@@ -35,20 +25,25 @@ var prompt = await File.ReadAllTextAsync("Plugin/prompt.txt");
 var instructionsLines = instructions.Split('\n', StringSplitOptions.RemoveEmptyEntries);
 var dataCsvLines = csvData.Split('\n', StringSplitOptions.RemoveEmptyEntries);
 
+var collection = memory.GetCollection<Guid, Artifacts<string>>("Artifacts");
+await collection.CreateCollectionIfNotExistsAsync();
 
 int idx = 0;
 foreach (var line in instructionsLines)
 {
-    await memory.SaveInformationAsync("instructions", line, $"{idx++}");
-    //kernel.Data.Add($"instructions-{idx++}", line);
+   
 }
 
 foreach (var line in dataCsvLines)
 {
-    await memory.SaveInformationAsync("data", line, $"{idx++}");
+   memoryStore.UpsertAsync
+    await kernel.Memory.SaveInformationAsync(
+        collectionName,
+        text: line,
+        id: $"data-{idx++}",
+        description: "Workspace data line"
+    );
 }
-
-
 
 Console.WriteLine("Embeddings stored in Milvus!");
 
